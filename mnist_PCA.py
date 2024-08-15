cf1940f6328ffb7bbcaad600d25730822cd8c856
Leilani Hoffmann <167572755+leilanihoffmann26@users.noreply.github.com>
2024-08-15 11:19:11 +0100

    Add files via upload
    Edits to ML algorithm performance analysis

--- mnist_PCA.py
+++ mnist_PCA.py
@@ -30,3 +30,25 @@ pca.fit(train_img)
 
 # find how many components PCA has after fitting the model
 print(pca.n_components_) # 95% of variance amounts to 330 principle components
+
+# apply mapping (transform) the training and testing sets
+train_img = pca.transform(train_img)
+test_img = pca.transform(test_img)
+
+# apply logistic regression to the transformed data
+from sklearn.linear_model import LogisticRegression
+# all parameters not specified are set to their defaults
+# default solver is incredibly slow which is why it was changed to 'lbfgs'
+logisticRegr = LogisticRegression(solver = 'lbfgs')
+logisticRegr.fit(train_img, train_lbl)
+
+# Predict for One Observation (image)
+logisticRegr.predict(test_img[0].reshape(1,-1))
+
+# Predict for Multiple Observations (images)
+logisticRegr.predict(test_img[0:10])
+
+# Measure model performance
+logisticRegr.score(test_img, test_lbl)
+# conclusion: PCA speeds up the fitting of ML algorithms
+
